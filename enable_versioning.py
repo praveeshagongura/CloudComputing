70aecc342f22105cd0e30ad634ff045917a8262e
Praveesha Gongura <66114853+praveeshagongura@users.noreply.github.com>
2025-02-19 02:05:12 -0500

    Add files via upload

---
+++ enable_versioning.py
@@ -0,0 +1,11 @@
+import boto3
+
+s3 = boto3.client('s3')
+bucket_name = 'my-boto3-s3-bucket-praveesha'
+
+s3.put_bucket_versioning(
+    Bucket=bucket_name,
+    VersioningConfiguration={'Status': 'Enabled'}
+)
+print(f'Versioning enabled for bucket {bucket_name}!')
+
